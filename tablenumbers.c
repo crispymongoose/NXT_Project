5a251de1f3f593d45fe42c7868a84bcaa04ee089
Sam Wong <awesomesillyawesome@gmail.com>
2012-11-19 16:50:44 -0500

    Uploaded Files
    Upload table identification

---
+++ tablenumbers.c
@@ -0,0 +1,16 @@
+int tableNum[7];
+
+void storeTables (sensorType[S1],
+{
+  sensorType[S1] = sensorCOLORFULL;
+  bool a = true;
+  motor[motorA] = 75;
+  while (a)
+  {
+    if (sensorType[S1] == 2) //intersection
+    {
+      //pick where to go
+    if (sensorType[S1] == 1) //border
+    {
+      //turn around and
+  }
